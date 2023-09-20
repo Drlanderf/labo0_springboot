7969d3c5dcf61549d276854c05631613f56c7073
landerf <lukas.paquay@outlook.be>
2023-09-20 11:47:06 +0200

    🎉 - Init commit

---
+++ src/test/java/com/example/labo0_springboot/Labo0SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labo0_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo0SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
